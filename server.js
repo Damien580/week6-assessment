4b45ebacc07cc5c840e22df853f311946ffe5f64
Damien Redd <damiens.redd@gmail.com>
2023-03-03 11:54:20 -0600

    hmmm

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.get('/api/robots/five', (req, res) => {
         let shuffled = shuffleArray(bots)
         let choices = shuffled.slice(0, 5)
         let compDuo = shuffled.slice(6, 8)
-        res.status(200).send({choices, compDuo})
         rollbar.log('Selections Displayed')
+        res.status(200).send({choices, compDuo})
     } catch (error) {
         console.log('ERROR GETTING FIVE BOTS', error)
         res.sendStatus(400)
